412893224734f6d71d560763603099c4b988a557
cielo-b <irumvaregisdmc@gmail.com>
2025-07-24 15:29:10 +0200

    [++]: added postgis in docker compse

--- src/land/dto/create-land.dto.ts
+++ src/land/dto/create-land.dto.ts
@@ -1,12 +1,20 @@
 import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+import { Column } from 'typeorm';
 
 export class CreateLandDto {
   @IsString()
   @IsNotEmpty()
   parcelNumber: string;
 
-  @IsNotEmpty()
-  location: any; // GeoJSON or WKT
+  @Column({ type: 'geometry', spatialFeatureType: 'Point', srid: 4326 })
+  location: any;
+
+  // {
+  //   "parcelNumber": "ABC-123",
+  //   "location": "SRID=4326;POINT(30.0619 -1.9441)",
+  //   "area": 1200,
+  //   "registeredOwnerId": 5,
+  // }
 
   @IsNumber()
   area: number;
